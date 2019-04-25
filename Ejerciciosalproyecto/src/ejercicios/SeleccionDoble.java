af4e9dc41968966c7833b7ab4fb63a2cb20a182c
SantiagoDGarcia <santiagodaivid@gmail.com>
2019-04-25 11:43:04 -0500

    EJERcambios25Abrl

--- Ejerciciosalproyecto/src/ejercicios/SeleccionDoble.java
+++ Ejerciciosalproyecto/src/ejercicios/SeleccionDoble.java
@@ -5,35 +5,44 @@ package ejercicios;
  */
 import java.util.Scanner;
 import paquetedos.Operacion;
-public class SeleccionSimple {
+public class SeleccionDoble {
 
 
     public static void main(String[] args) {
-        //documentacion
+        
+        //desarrollo...
         
         Scanner entrada = new Scanner(System.in);
         
         int calificacion;
         
         String miMensaje = Operacion.mensaje;
+        String miMensaje2 = Operacion.mensaje2;
         
-        System.out.println("Ingrese la calificacion número 1:");
+        System.out.println("Ingrese la calificación número 1:");
         calificacion = entrada.nextInt();
         
         if (calificacion >= 85){
             System.out.printf("%s %d\n", miMensaje, calificacion);
+        } else{
+            System.out.printf("%s %d\n", miMensaje2, calificacion);
         } 
         
         int calificacion_2;
         
-        System.out.println("Ingrese la calificacion número 2:");
+        System.out.println("Ingrese la calificación número 2:");
         calificacion_2 = entrada.nextInt();
         
-        if (calificacion_2 >= 85 ) {
-             System.out.printf("Usted esta aprobado con %d\n", calificacion_2);
+        
+        if (calificacion_2 >= 85){
+            System.out.printf("%s %d\n", miMensaje, calificacion_2);
+        } else{
+            System.out.printf("%s %d\n", miMensaje2, calificacion_2);
         }
+        
+       }
     }
-   }
+   
     
     
 
--- Ejerciciosalproyecto/src/paquetedos/Operacion.java
+++ Ejerciciosalproyecto/src/paquetedos/Operacion.java
@@ -6,8 +6,7 @@ package paquetedos;
 
 public class Operacion {
     
-    public static String mensaje = "Usted esta aprobado con";
-    
-    
+    public static String mensaje = "Usted esta aprobado con ";
+    public static String mensaje2 = "Usted esta reprobado con ";
     
 }
