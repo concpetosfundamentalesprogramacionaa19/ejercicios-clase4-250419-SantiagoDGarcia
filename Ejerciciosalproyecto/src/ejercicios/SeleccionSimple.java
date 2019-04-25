d7d919c3bbbff48aa751754ae4d81fd94602eb6c
SantiagoDGarcia <santiagodaivid@gmail.com>
2019-04-25 11:29:00 -0500

    ejercicios25abr

---
+++ Ejerciciosalproyecto/src/ejercicios/SeleccionSimple.java
@@ -0,0 +1,39 @@
+package ejercicios;
+/**
+ *
+ * @author Sant Garcia
+ */
+import java.util.Scanner;
+import paquetedos.Operacion;
+public class SeleccionSimple {
+
+
+    public static void main(String[] args) {
+        //documentacion
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int calificacion;
+        
+        String miMensaje = Operacion.mensaje;
+        
+        System.out.println("Ingrese la calificacion número 1:");
+        calificacion = entrada.nextInt();
+        
+        if (calificacion >= 85){
+            System.out.printf("%s %d\n", miMensaje, calificacion);
+        }
+        
+        int calificacion_2;
+        
+        System.out.println("Ingrese la calificacion número 2:");
+        calificacion_2 = entrada.nextInt();
+        
+        if (calificacion_2 >= 85 ) {
+             System.out.printf("Usted esta aprobado con %d\n", calificacion_2);
+        }
+    }
+   }
+    
+    
+
---
+++ Ejerciciosalproyecto/src/paquetedos/Operacion.java
@@ -0,0 +1,13 @@
+package paquetedos;
+/**
+ *
+ * @author Sant Garcia
+ */
+
+public class Operacion {
+    
+    public static String mensaje = "Usted esta aprobado con";
+    
+    
+    
+}
